1fd366e85bca24c06acbad1f96eae523c58b352b
lizhixiong <lzx7421@hotmail.com>
2019-05-11 01:37:44 +0800

    first create

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# trash-road
+# trash-road（TR）
 a small game for cleaning the trash on road.
+### 文件说明
+### design
+
+此文件夹存放游戏总体说明性内容。
+
+主要是游戏文档、设计文档、数据表等等。
+
+另外，其他项目相关含有说明形式的文件存放在此文件夹下。
+
+### project
+程序相关工程，文件等。
---
+++ project/TrashRoad/Assets/Scripts/RoadCreator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadCreator : MonoBehaviour
+{
+    public GameObject temp;
+    public int count = 5;
+    void Start()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject go = GameObject.Instantiate(temp);
+            go.transform.SetParent(transform, false);
+            Vector3 pos = Vector3.forward * i * 24;
+            go.transform.localPosition = pos;
+        }
+    }
+}
