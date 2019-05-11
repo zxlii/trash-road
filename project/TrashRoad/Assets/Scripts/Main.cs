c3618f5d15cb1132eaeec7d0ee36627b10fc08b6
lizhixiong <lzx7421@hotmail.com>
2019-05-11 17:26:42 +0800

    change

--- project/TrashRoad/Assets/Scripts/Main.cs
+++ project/TrashRoad/Assets/Scripts/Main.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Main : MonoBehaviour {
+public class Main : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
